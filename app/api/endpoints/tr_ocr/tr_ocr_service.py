cf428101e0c7cb5b6293ba8e8040674b27369c3b
SovansunchhayKhoun <sunchhay395@gmail.com>
2025-03-27 09:39:53 +0700

    feat: refactor file processing logic to separate image processing and visualization functions

--- app/api/endpoints/tr_ocr/tr_ocr_service.py
+++ app/api/endpoints/tr_ocr/tr_ocr_service.py
@@ -10,11 +10,71 @@ from app.api.endpoints.tr_ocr.schema import (
     FileMetadata,
     Prediction,
     TrOcrImagePredictionResponseModel,
+    TrOcrPredictionResponseModel,
 )
 import torch
 
 
-async def process_image_file(file: UploadFile) -> TrOcrImagePredictionResponseModel:
+async def process_file(file: UploadFile) -> TrOcrPredictionResponseModel:
+    """Processes a single file and returns a TrOcrPredictionResponseModel."""
+    try:
+        image_bytes = await file.read()
+        image = cv2.imdecode(np.frombuffer(image_bytes, np.uint8), cv2.IMREAD_COLOR)
+
+        results = yolo_model(image)
+
+        predictions = []
+        cropped_images_list = []
+        bounding_boxes = []
+
+        for result in results:
+            boxes = result.boxes.xyxy.numpy()
+            for box in boxes:
+                x, y, width, height = map(int, box)
+                cropped_image = image[y:height, x:width]
+                cropped_images_list.append(
+                    Image.fromarray(cv2.cvtColor(cropped_image, cv2.COLOR_BGR2RGB))
+                )
+                bounding_boxes.append(
+                    {
+                        "x": float(box[0]),
+                        "y": float(box[1]),
+                        "width": float(box[2]),
+                        "height": float(box[3]),
+                    }
+                )
+
+        # Batched TrOCR Inference
+        if cropped_images_list:
+            pixel_values = processor(
+                images=cropped_images_list, return_tensors="pt"
+            ).pixel_values
+
+            with torch.no_grad():
+                generated_ids = trocr_model.generate(pixel_values)
+            generated_texts = processor.batch_decode(
+                generated_ids, skip_special_tokens=True
+            )
+
+            for i, text in enumerate(generated_texts):
+                predictions.append(Prediction(text=text, boundingBox=bounding_boxes[i]))
+
+        file_metadata = FileMetadata(
+            fileName=file.filename, fileSize=file.size, fileType=file.content_type
+        )
+
+        return TrOcrPredictionResponseModel(
+            fileMetadata=file_metadata, predictions=predictions
+        )
+
+    except Exception as e:
+        print(e)
+        raise HTTPException(status_code=500, detail=str(e))
+
+
+async def process_and_visualize_file(
+    file: UploadFile,
+) -> TrOcrImagePredictionResponseModel:
     """Processes a single file and returns a TrOcrPredictionResponseModel."""
     try:
         image_bytes = await file.read()
